eee49e44cad608fcb714965eec7cd2b94073312f
CyberJunkie <51170081+cyber-junkie@users.noreply.github.com>
2019-10-09 15:56:21 -0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,8 @@
+By CyberJunkie
+
+Look me up on Youtube 
+
+
+The script is only for educational purposes. I'm not liable for the misuse of this script in anyway.
+
+rotaTOR is a bash script that simply restarts TOR any given seconds entered in the script. Note that entering any number less than 45 seconds is not recommended as TOR and sites on the network have greater latency and could result in timouts.
---
+++ rotaTOR.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# https://github.com/cyber-junkie/rotaTOR by CyberJunkie
+#
+# 
+
+printf "To exit \e[32m rotaTOR \e[0m press \e[33m CRTL+C \e[0m\n"
+
+#TOR check
+
+echo -e "Lets check for Tor on this system.\n"
+	tor="/etc/init.d/tor"
+
+
+if [ -f $tor ];
+	then
+		echo -e "Tor > installed\n\n"
+		echo -e "Starting Tor\n\n"
+service tor start
+	else
+		echo -e "Tor is missing! Please run apt update && apt install tor\n"
+	exit 
+fi
+
+
+# Timer based on seconds for TOR restart
+
+	echo -e "Enter number of seconds before new IP: \c"
+
+		read timer
+
+if [ "$timer" -lt "45" ];
+	then
+		printf " \e[31m Warning!!! \e[0m Less than 45 seconds is not good due to latency and timouts\n"
+		echo -e "Fine, as you wish :P \n"
+	else
+		echo -e "Starting timer \n"
+fi
+
+function control_c {
+	echo -en "\nWait, Did you cover your tracks...jk\n"
+service tor stop
+	echo -en "You no longer using TOR\n"
+exit $?
+}
+
+# ctrl+c and exit rotaTOR.
+	trap control_c SIGINT
+
+
+for (( ; ; ))
+	do 
+		sleep $timer
+service tor restart
+		echo -e "Grabbed a new IP, New IP coming in $timer \n"
+done
+
+
