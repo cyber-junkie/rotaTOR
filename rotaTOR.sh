4bb468f269d5756ed74b9628b54a6fa25bd69fe8
CyberJunkie <51170081+cyber-junkie@users.noreply.github.com>
2019-10-16 09:32:41 -0700

    Update rotaTOR.sh

--- rotaTOR.sh
+++ rotaTOR.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-#                       __              ________  ______   _______                 __       
-#                      /  |            /        |/      \ /       \               /  |      
-#  ______    ______   _$$ |_     ______$$$$$$$$//$$$$$$  |$$$$$$$  |      _______ $$ |____  
-# /      \  /      \ / $$   |   /      \  $$ |  $$ |  $$ |$$ |__$$ |     /       |$$      \ 
-#/$$$$$$  |/$$$$$$  |$$$$$$/    $$$$$$  | $$ |  $$ |  $$ |$$    $$<     /$$$$$$$/ $$$$$$$  |
-#$$ |  $$/ $$ |  $$ |  $$ | __  /    $$ | $$ |  $$ |  $$ |$$$$$$$  |    $$      \ $$ |  $$ |
-#$$ |      $$ \__$$ |  $$ |/  |/$$$$$$$ | $$ |  $$ \__$$ |$$ |  $$ | __  $$$$$$  |$$ |  $$ |
-#$$ |      $$    $$/   $$  $$/ $$    $$ | $$ |  $$    $$/ $$ |  $$ |/  |/     $$/ $$ |  $$ |
-#$$/        $$$$$$/     $$$$/   $$$$$$$/  $$/    $$$$$$/  $$/   $$/ $$/ $$$$$$$/  $$/   $$/ 
-# https://github.com/cyber-junkie/rotaTOR by CyberJunkie
-#
-# 
+cat << "EOF"
+		       __              ________  ______   _______                 __       
+                      /  |            /        |/      \ /       \               /  |      
+  ______    ______   _$$ |_     ______$$$$$$$$//$$$$$$  |$$$$$$$  |      _______ $$ |____  
+ /      \  /      \ / $$   |   /      \  $$ |  $$ |  $$ |$$ |__$$ |     /       |$$      \ 
+/$$$$$$  |/$$$$$$  |$$$$$$/    $$$$$$  | $$ |  $$ |  $$ |$$    $$<     /$$$$$$$/ $$$$$$$  |
+$$ |  $$/ $$ |  $$ |  $$ | __  /    $$ | $$ |  $$ |  $$ |$$$$$$$  |    $$      \ $$ |  $$ |
+$$ |      $$ \__$$ |  $$ |/  |/$$$$$$$ | $$ |  $$ \__$$ |$$ |  $$ | __  $$$$$$  |$$ |  $$ |
+$$ |      $$    $$/   $$  $$/ $$    $$ | $$ |  $$    $$/ $$ |  $$ |/  |/     $$/ $$ |  $$ |
+$$/        $$$$$$/     $$$$/   $$$$$$$/  $$/    $$$$$$/  $$/   $$/ $$/ $$$$$$$/  $$/   $$/ 
+               https://github.com/cyber-junkie/rotaTOR by CyberJunkie
+EOF
+
+
 
 printf "To exit \e[32m rotaTOR \e[0m press \e[33m CRTL+C \e[0m\n"
 
@@ -22,8 +24,8 @@ echo -e "Lets check for Tor on this system.\n"
 
 if [ -f $tor ];
 	then
-		echo -e "Tor > installed\n\n"
-		echo -e "Starting Tor\n\n"
+		echo -e "Tor is installed\n\n"
+		echo -e "Starting Tor Service\n\n"
 service tor start
 	else
 		echo -e "Tor is missing! Please run apt update && apt install tor\n"
@@ -33,14 +35,14 @@ fi
 
 # Timer based on seconds for TOR restart
 
-	echo -e "Enter number of seconds before new IP: \c"
+	echo -e "Enter number of seconds (It is recommmended to choose more than 45 sec) before new IP: \c"
 
 		read timer
 
 if [ "$timer" -lt "45" ];
 	then
-		printf " \e[31m Warning!!! \e[0m Less than 45 seconds is not good due to latency and timouts\n"
-		echo -e "Fine, as you wish :P \n"
+		printf " \e[31m Warning!!! \e[0m Less than 45 seconds is not recommended due to latency on the TOR network and could cause timouts\n"
+		echo -e "OK, lets proceed anyway :P \n"
 	else
 		echo -e "Starting timer \n"
 fi
